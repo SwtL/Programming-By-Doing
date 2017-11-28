6e5abd2108a7059f72bab4d807e13be5e591c26b
Tom <34043667+SwtL@users.noreply.github.com>
2017-11-28 18:00:57 +0000

    Add files via upload

---
+++ For Loops/TenTimes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TenTimes {
+    public static void main(String[] args) {
+
+        String message = "Mr. Mitchell is cool.";
+
+        for (int n = 1; n <= 10; n = n + 1) {
+            System.out.println(n + ". " + message);
+        }
+    }
+}
